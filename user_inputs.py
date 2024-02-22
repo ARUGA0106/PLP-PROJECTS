498f71542a2b9ccc89e5d432bb2a25682da61dfd
ARUGA0106 <aruga0106@gmail.com>
2024-02-22 17:14:04 +0300

    code to collect and print  user inputs

---
+++ user_inputs.py
@@ -0,0 +1,15 @@
+# simple python code  to collect user inputs
+
+# Asking  a user for the name
+
+name = input("What is your name? ")
+
+# Asking a user for the age
+
+age = input("How old are you? ")
+
+# Asking a  user for the location
+location = input("Where are you located? ")
+
+# Print personalized message
+print(f"Hello {name}, you are {age} years old and live  at {location}.")
